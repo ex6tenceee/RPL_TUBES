ea480541c080265d410b096e5516a422db31a902
yanuarfrd <yanuarfrds@365.telkomuniversity.ac.id>
2022-04-18 23:24:21 +0700

    Menambahkan code button 2 untuk pindah ke menu registrasi

--- Form1.cs
+++ Form1.cs
@@ -50,5 +50,12 @@ namespace TUBESRPL_inputBuku
                 }
             }
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form3 bb = new Form3();
+            bb.ShowDialog();
+        }
     }
 }
