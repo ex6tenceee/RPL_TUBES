67a2ea94904c5ca384bf511a190932da3890577c
yanuarfrd <yanuarfrds@365.telkomuniversity.ac.id>
2022-04-18 22:24:22 +0700

    Project Tubes KPL

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace TUBESRPL_inputBuku
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
