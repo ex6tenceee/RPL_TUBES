ec76a24a44bc71454bc9acb9533df77bf8870b1f
abdulghofur12 <ghofursans12@gmail.com>
2022-04-19 22:30:11 +0700

    menambahkan json file dll

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -40,39 +40,43 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(12, 91);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(130, 20);
+            this.textBox1.Size = new System.Drawing.Size(233, 20);
             this.textBox1.TabIndex = 0;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(373, 159);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(373, 528);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(158, 23);
+            this.button1.Size = new System.Drawing.Size(209, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "cek";
+            this.button1.Text = "input";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(212, 91);
+            this.textBox2.Location = new System.Drawing.Point(12, 197);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(121, 20);
+            this.textBox2.Size = new System.Drawing.Size(233, 20);
             this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(394, 91);
+            this.textBox3.Location = new System.Drawing.Point(12, 294);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(116, 20);
+            this.textBox3.Size = new System.Drawing.Size(233, 20);
             this.textBox3.TabIndex = 3;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // label1
             // 
@@ -80,23 +84,26 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(368, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(142, 25);
+            this.label1.Size = new System.Drawing.Size(122, 25);
             this.label1.TabIndex = 4;
-            this.label1.Text = "update buku";
+            this.label1.Text = "input buku";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(577, 91);
+            this.textBox4.Location = new System.Drawing.Point(12, 387);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(125, 20);
+            this.textBox4.Size = new System.Drawing.Size(233, 20);
             this.textBox4.TabIndex = 5;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(775, 91);
+            this.textBox5.Location = new System.Drawing.Point(12, 499);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(124, 20);
+            this.textBox5.Size = new System.Drawing.Size(233, 20);
             this.textBox5.TabIndex = 6;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
             // 
             // label2
             // 
@@ -110,7 +117,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(209, 69);
+            this.label3.Location = new System.Drawing.Point(12, 181);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(59, 13);
             this.label3.TabIndex = 8;
@@ -119,7 +126,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(391, 69);
+            this.label4.Location = new System.Drawing.Point(12, 278);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(45, 13);
             this.label4.TabIndex = 9;
@@ -128,7 +135,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(574, 69);
+            this.label5.Location = new System.Drawing.Point(12, 371);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(63, 13);
             this.label5.TabIndex = 10;
@@ -137,17 +144,27 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(772, 69);
+            this.label6.Location = new System.Drawing.Point(12, 459);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(40, 13);
             this.label6.TabIndex = 11;
             this.label6.Text = "penulis";
             // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(554, 294);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 24);
+            this.label7.TabIndex = 12;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(923, 580);
+            this.ClientSize = new System.Drawing.Size(934, 580);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -161,7 +178,9 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -181,6 +200,7 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,30 +1,149 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
 using System.Windows.Forms;
 
 namespace TUBESRPL_inputBuku
 {
     public partial class Form1 : Form
     {
+        private object book;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "" || textBox4.Text == "" || textBox5.Text == "")
+            {
+                MessageBox.Show("Lengkapi data terlebih dahulu.!", "Message");
+            }
+            else
+            {
+                MessageBox.Show("berhasil di tambahkan.!","message");
+            }
+            
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
 
-        private void button1_Click(object sender, EventArgs e)
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
     }
+
+    public class InputBukuConfig
+    {
+        public Buku Config;
+        public string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+        public string fileConfigName = "Input_Buku_Config.json";
+
+        public InputBukuConfig()
+        {
+            try
+            {
+               
+                ReadConfigFile();
+            }
+            catch (Exception)
+            {
+                SetDefault();
+                WriteNewConfigFile();
+            }
+        }
+
+        private void ReadConfigFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Buku ReadConfigFile(string jsonStringFromFile)
+        {
+            string jsonStringFromFile = File.ReadAllText(path + "/" + fileConfigName);
+            Config = JsonSerializer.Deserialize<Buku>(jsonStringFromFile);
+            return Config ;
+        }
+
+        private void SetDefault()
+        {
+            int id_buku = 14325;
+            string judul_buku = "manusia setengah dewa";
+            string penerbit = "pt.percetakaan jaya abadi";
+            int tahun_terbit = 2018;
+            string penulis = "mangku alam";
+
+        }
+
+        private void WriteNewConfigFile()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+
+            string jsonString = JsonSerializer.Serialize(Config, options);
+            string fullFilePath = path + "/" + fileConfigName;
+            File.WriteAllText(fullFilePath, jsonString);
+        }
+
+       public class Buku
+        {
+        public int id_buku { get; set; }
+        public string judul_buku { set; get; }
+        public string penerbit { set; get; }
+        public int tahun_terbit { set; get; }
+        public string penulis { set; get; }
+        public pesanAwal pesanAwal { set; get; }
+        
+
+        public Buku (int id_buku, string judul_buku, string penerbit, int tahun_terbit, string penulis,pesanAwal pesanAwal)
+        {
+            this.id_buku = id_buku;
+            this.judul_buku = judul_buku;
+            this.penerbit=penerbit;
+            this.tahun_terbit = tahun_terbit;
+            this.penulis = penulis;
+            this.pesanAwal = pesanAwal;
+        }
+       }
+
+
+     class pesanAwal
+    {
+        private string en { get; set; }
+        private string id { get; set; }
+    }
+
+
+
 }
